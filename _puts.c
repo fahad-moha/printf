81c9c8980d14a1c9394d37f38e016d420ee6ca50
fahad-moha <khiladifahad02@gmail.com>
2023-08-20 06:31:40 -0700

    updated

--- _puts.c
+++ _puts.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stddef.h>
+#include <unistd.h>
 
 /**
  * _puts - prints a string to stdout
@@ -10,12 +11,42 @@
 int _puts(char *str)
 {
 	int count = 0;
+	unsigned char ch = *str;
 
 	if (str == NULL)
-		str = "(null)";
+		return (_puts("(null)"));
 	while (*str)
 	{
-		count += _putchar(*str);
+		if (ch < 128)
+			count += _putchar(ch);
+		else
+		{
+			if ((ch & 0xE0) == 0xC0)
+			{
+				count += _putchar(ch);
+				count += _putchar(*(str + 1));
+				str++;
+			}
+			else if ((ch & 0xF0) == 0xE0)
+			{
+				count += _putchar(ch);
+				count += _putchar(*(str + 1));
+				count += _putchar(*(str + 2));
+				str += 2;
+			}
+			else if ((ch & 0xF8) == 0xF0)
+			{
+				count += _putchar(ch);
+				count += _putchar(*(str + 1));
+				count += _putchar(*(str + 2));
+				count += _putchar(*(str + 3));
+				str += 3;
+			}
+			else
+			{
+				count += _putchar('?');
+			}
+		}
 		str++;
 	}
 	return (count);
